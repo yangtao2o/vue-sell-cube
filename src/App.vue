182b92c7b242c51fec351091c02a877b5b26b5b7
yangtao <yangtaobanner@foxmail.com>
2018-12-05 17:08:33 +0800

    header-details

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import qs from 'query-string'
+import VHeader from 'components/v-header/v-header'
+import { getSeller } from 'api'
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      seller: {
+        id: qs.parse(location.search).id
+      }
+    }
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
+  },
   components: {
-    HelloWorld
+    VHeader
   }
+
 }
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const webpack = require('webpack')
+const path = require('path')
+const appData = require('./data.json')
+const seller = appData.seller
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -14,5 +23,26 @@ module.exports = {
       postCompile: true,
       theme: true
     }
-  }
+  },
+  devServer: {
+    before (app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+    }
+  },
+  chainWebpack (config) {
+    config.resolve.alias
+      .set('components', resolve('src/components/'))
+      .set('common', resolve('src/common'))
+      .set('api', resolve('src/api'))
+
+    config.plugin('context')
+      .use(webpack.ContextReplacementPlugin,
+        [/moment[/\\]locale$/, /zh-cn/])
+  },
+  baseUrl: ''
 }
